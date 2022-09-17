1739b6b4eb55a84c80758a82751d5278ae361e0b
郝飞宇 <2517868798@qq.com>
2022-09-17 22:09:06 +0800

    basic

---
+++ provider-service/src/main/java/com/lagou/providerservice/controller/ProviderController.java
@@ -0,0 +1,12 @@
+package com.lagou.providerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderController {
+    @GetMapping("/provider/msg")
+    public String sendMessage() {
+        return "This is the message from provider service!";
+    }
+}
